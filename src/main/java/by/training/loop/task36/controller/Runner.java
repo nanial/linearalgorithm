da43cea34b4047d25823483e0b284215c6b6bd8a
nanial <nanial@mail.ru>
2019-12-14 10:56:11 +0300

    conditional & loop

--- src/main/java/by/training/loop/task36/controller/Runner.java
+++ src/main/java/by/training/loop/task36/controller/Runner.java
@@ -3,14 +3,26 @@ package by.training.loop.task36.controller;
 //Два двузначных числа, записанных одно за другим , образуют четырёхзначное
 //число, которое делится на их произведение. Найти эти числа
 
+
 public class Runner {
+
     public static void main(String[] args) {
 
         int [] arr = new int[100];
+
         for (int i = 10; i < arr.length; i++) {
             arr[i] = i;
-            System.out.println(arr[i]);
         }
+        for (int i = 10; i < arr.length ; i++) {
 
+            for (int j = 10; j < arr.length; j++) {
+
+                int temp = arr[i] * 100 + arr[j];
+
+                if (temp % (arr[i] * arr[j]) == 0) {
+                    System.out.println(arr[i] + "" + arr[j]);
+                }
+            }
+        }
     }
 }

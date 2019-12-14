ec54fc484065590561ff60fcce3a5c6cb5802282
nanial <nanial@mail.ru>
2019-12-14 13:01:49 +0300

    next day

--- src/main/java/by/training/condition/mycalendar/action/TomorrowExec.java
+++ src/main/java/by/training/condition/mycalendar/action/TomorrowExec.java
@@ -1,5 +1,38 @@
 package by.training.condition.mycalendar.action;
 
+import by.training.condition.mycalendar.domain.MyDate;
+import by.training.condition.mycalendar.validator.DateValidator;
+
 public class TomorrowExec {
 
+    public MyDate getNextDay(int day, int month, int year){
+
+        DateValidator dateValidator = new DateValidator();
+        MyDate nextDay = new MyDate();
+        if(dateValidator.isValidDate(day, month, year)) {
+
+            if (dateValidator.isValidDate(day + 1, month, year)) {
+                nextDay.setDay(day + 1);
+                nextDay.setMonth(month);
+                nextDay.setYear(year);
+                return nextDay;
+            } else {
+                if (dateValidator.isValidDate(1, month + 1, year)) {
+                    nextDay.setDay(1);
+                    nextDay.setMonth(month + 1);
+                    nextDay.setYear(year);
+                    return nextDay;
+                } else {
+                    nextDay.setDay(1);
+                    nextDay.setMonth(1);
+                    nextDay.setYear(year + 1);
+                    return nextDay;
+                }
+            }
+        }
+        else {
+            System.out.println("Your date is incorrect!");
+        }
+        return nextDay;
+    }
 }
--- src/main/java/by/training/condition/mycalendar/controller/Runner.java
+++ src/main/java/by/training/condition/mycalendar/controller/Runner.java
@@ -1,11 +1,15 @@
 package by.training.condition.mycalendar.controller;
 
 import by.training.condition.mycalendar.action.ReadDate;
+import by.training.condition.mycalendar.action.TomorrowExec;
+import by.training.condition.mycalendar.domain.MyDate;
 
 public class Runner {
     public static void main(String[] args) {
 
-        System.out.println((new DateCreator()
-                .createValidDate(new ReadDate().getDate())).toString());
+        MyDate today = new DateCreator().createValidDate(new ReadDate().getDate());
+        System.out.println(today.toString());
+        System.out.println(new TomorrowExec()
+                .getNextDay(today.getDay(), today.getMonth(), today.getYear()).toString());
     }
 }
